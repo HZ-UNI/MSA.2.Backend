d5f96385b59009fb6f46db92bf729904bf5e2993
Haichuan Zhang <hzha684@aucklanduni.ac.nz>
2022-08-10 09:44:56 +1200

    Add project files.

---
+++ MSA.2.Backend/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSA._2.Backend.Models;
+using System.Diagnostics;
+
+namespace MSA._2.Backend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MSA.2.Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSA._2.Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
